e4ec8d8db3255d6b0ac4c7baaf2e2d5efb1a849d
Eric <eric2umeh@yahoo.com>
2023-03-14 10:11:32 +0100

    Add Router and Layout to App.js & index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/layout/Layout';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details/:cc" element={<HomePage />} />
+      </Routes>
+    </Layout>
   );
 }
 
