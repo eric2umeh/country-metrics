693ab501196b373cc915b87f078e962d94b4aee8
Eric <eric2umeh@yahoo.com>
2023-03-14 15:29:30 +0100

    Create store.js in redux

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import countries from './countries/countries';
+
+const store = configureStore({
+  reducer: { countries },
+});
+
+export default store;
