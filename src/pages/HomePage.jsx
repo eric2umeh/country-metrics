8744f96ddb3af25473cc4f81cae5a695e7f7d003
Eric <eric2umeh@yahoo.com>
2023-03-14 16:05:19 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Countries from '../components/countries/Countries';
 
 const HomePage = () => (
-  <div>HomePage</div>
+  <section>
+    <Countries />
+  </section>
 );
 
 export default HomePage;
